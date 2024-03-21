56ae044c0e124a0443c7dc001b23d91463cf739d
donggun08 <164193395+donggun08@users.noreply.github.com>
2024-03-21 11:35:36 +0900

    Add files via upload
    중첩if문

--- FileName.c
+++ FileName.c
@@ -1,19 +1,32 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main(void) {
 
+	int a, b, c;
+	printf("세 정수를 입력하세요:");
+	scanf_s("%d %d %d", &a, &b, &c );
 
-	int answer = 0;
-	
-	for (int i = 0; i <= 1000; i++)
-		answer += i;
-
-	printf("1부터 1000까지 총 숫자의 합은 %d 입니다", answer);
-
-
-
-
-
+	if (a > b && a > c) {
+		if (b > c)
+			printf("제일 큰 값은 a=%d 중간값은 b=%d 제일 작은 값은c=%d",a,b,c);
+		else
+			printf("제일 큰 값은 a=%d 중간값은 c=%d 제일 작은 값은 b=%d",a,c,b);
+	}
+	if (c > a && c > b) {
+		if (a > b)
+			printf("제일 큰 값은 c=%d 중간 값은 a=%d 제일 작은 값은 b=%d",c,a,b);
+		else
+			printf("제일 큰 값은 c=%d 중간 값은 b=%d 제일 작은 값은 a=%d",c,b,a);
+	}
+	if (b > a && b > c) {
+		if (a > c)
+			printf("제일 큰 값은 b=%d 중간 값은 a=%d 제일 작은 값은 c=%d",b,a,c);
+		else
+			printf("제일 큰 값은 b=%d 중간 값은 c=%d 제일 작은 값은 a=%d",b,c,a);
+	}
 
 	return 0;
 }
+
+//첫번째로 큰 if문을 만들어서 가장 큰 값을 정하고 그 안에서 중첩 if문을 써서 두번째 값을 구하고 그것이 아닐것을 대비해서 else문을 만들어서
+//  6가지 경우의 수를 만들어야한다
