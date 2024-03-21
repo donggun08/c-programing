96a225653a08ecd2a5e31590fcca43f5bd9c759e
donggun08 <164193395+donggun08@users.noreply.github.com>
2024-03-22 08:16:18 +0900

    Add files via upload
    if문 과 and연산자 활용

--- FileName.c
+++ FileName.c
@@ -2,31 +2,20 @@
 
 int main(void) {
 
-	int a, b, c;
-	printf("세 정수를 입력하세요:");
-	scanf_s("%d %d %d", &a, &b, &c );
+	int num1, num2, num3, answer;
 
-	if (a > b && a > c) {
-		if (b > c)
-			printf("제일 큰 값은 a=%d 중간값은 b=%d 제일 작은 값은c=%d",a,b,c);
-		else
-			printf("제일 큰 값은 a=%d 중간값은 c=%d 제일 작은 값은 b=%d",a,c,b);
-	}
-	if (c > a && c > b) {
-		if (a > b)
-			printf("제일 큰 값은 c=%d 중간 값은 a=%d 제일 작은 값은 b=%d",c,a,b);
-		else
-			printf("제일 큰 값은 c=%d 중간 값은 b=%d 제일 작은 값은 a=%d",c,b,a);
-	}
-	if (b > a && b > c) {
-		if (a > c)
-			printf("제일 큰 값은 b=%d 중간 값은 a=%d 제일 작은 값은 c=%d",b,a,c);
-		else
-			printf("제일 큰 값은 b=%d 중간 값은 c=%d 제일 작은 값은 a=%d",b,c,a);
-	}
+	printf("세 개의 정수를 입력하시오");
+	scanf_s("%d %d %d",&num1, &num2, &num3 );
 
+	if (num1 > num2 && num2 > num3) {
+		printf("%d %d %d", num1, num2, num3);
+	}
+	if (num2 > num3 && num3 > num1) {
+		printf("%d %d %d", num2, num3, num1);
+	}
+	if (num3 > num2 && num2 > num1) {
+		printf("%d %d %d", num1, num2, num3);
+	}
+	
 	return 0;
 }
-
-//첫번째로 큰 if문을 만들어서 가장 큰 값을 정하고 그 안에서 중첩 if문을 써서 두번째 값을 구하고 그것이 아닐것을 대비해서 else문을 만들어서
-//  6가지 경우의 수를 만들어야한다
